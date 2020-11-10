3035942b4fd7678691b5d9662bb0a7b1a1b028a2
txy <656877449@qq.com>
2020-11-10 16:48:29 +0800

    新增或修改

--- src/page/more/index.vue
+++ src/page/more/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="main">
     <div class="header">
-      <div class="back" @click="route_to('/')"></div>
+      <div class="back" v-on:click="route_to('/')"></div>
       频道管理
     </div> 
     <div class="middle">
       <div class="title">点击删除以下频道</div>
       <div class="content">
-        <div class="item"></div>
+        <div class="option" v-for="(item, index) of option_delete" :key="index" v-bind:style="item.option_name_style">{{item.option_name}}</div>
       </div>
     </div>
-    <div class="bottom"></div>
+    <div class="bottom">
+      <div class="title">点击添加以下频道</div>
+      <div class="content">
+        <div class="option" v-for="(item, index) of option_add" :key="index">{{item.option_name}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,7 +24,71 @@ export default {
   name: 'more',
   data() {
     return {
-      name: 'helloworld vue'
+      option_delete: [
+        {
+          option_name: "推荐",
+          option_name_style: "background: #f0f0f0;"
+        },
+        {
+          option_name: "热点"
+        },
+        {
+          option_name: "社会"
+        },
+        {
+          option_name: "娱乐"
+        },
+        {
+          option_name: "科技"
+        },
+        {
+          option_name: "汽车"
+        },
+        {
+          option_name: "体育"
+        },
+        {
+          option_name: "财经"
+        },
+        {
+          option_name: "军事"
+        },
+        {
+          option_name: "国际"
+        },
+        {
+          option_name: "时尚"
+        },
+        {
+          option_name: "游戏"
+        },
+      ],
+      option_add: [
+        {
+          option_name: "旅游"
+        },
+        {
+          option_name: "历史"
+        },
+        {
+          option_name: "探索"
+        },
+        {
+          option_name: "美食"
+        },
+        {
+          option_name: "育儿"
+        },
+        {
+          option_name: "养生"
+        },
+        {
+          option_name: "故事"
+        },
+        {
+          option_name: "美文"
+        },
+      ]
     }
   },
   methods: {
@@ -42,6 +111,9 @@ export default {
 </script>
 
 <style lang="less" scoped>
+body {
+  background: #f8f8f8;
+}
 .main {
   .header {
     width: 100%;
@@ -62,6 +134,31 @@ export default {
       position: absolute;
       top: 0;
       left: 0;
+    }
+  }
+  .middle, .bottom {
+    .title {
+      font-size: 12px;
+      color: #666;
+      font-weight: 400;
+      padding: 4px 8px;
+    }
+    .content {
+      width: 100%;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+      margin: 0 0 10px 0;
+      padding: 0 2px;
+      .option {
+        font-size: 16px;
+        color: #131313;
+        font-weight: 400;
+        border: 1px solid #ccc;
+        height: 34px;
+        text-align: center;
+        line-height: 32px;
+        margin: 7px 6px;
+      }
     }
   }
 }
