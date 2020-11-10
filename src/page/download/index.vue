9080e39f8cd4b86921d3a107a508a1915937daa9
txy <656877449@qq.com>
2020-11-10 17:43:27 +0800

    新增或修改

--- src/page/download/index.vue
+++ src/page/download/index.vue
@@ -1,8 +1,9 @@
 <template>
-  	<div>
-    	This is from route page 
-      <div>{{ name | capitalize }}</div>
-  	</div>
+  <div class="main">
+    <div class="photo">
+      <div class="button">点击下载</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -27,6 +28,37 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    
+.main {
+  position: relative;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  .photo {
+    background: url(https://s1.pstatp.com/growth/mobile_landingpage/images/bg-wap.9710384a.png);
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    .button {
+      border: 1px solid #56bfff;
+      width: calc(100% - 30px);
+      height: 37px;
+      position: fixed;
+      top: 80%;
+      text-align: center;
+      font-size: 12px;
+      line-height: 34px;
+      background: #56bfff;
+      color: #fff;
+      box-shadow: 
+      0 0 0 1px #2fb0ff, 
+      inset 0 2px 0 hsla(0,0%,100%,0.2), 
+      0 1px 0 1px rgba(0,0,0,0.2);
+      border-radius: 3px;
+      font-weight: 400;
+    }
+  }
+}
 
 </style>
