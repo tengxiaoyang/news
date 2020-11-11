474239e006aea235b3c157efdba228cc38ac4342
txy <656877449@qq.com>
2020-11-11 11:14:50 +0800

    新增或修改

--- src/page/news_details/index.vue
+++ src/page/news_details/index.vue
@@ -5,7 +5,7 @@
       <span>今日头条</span>
       <div class="button">打开</div>
     </div>
-    <div class="news">
+    <div class="news_detail">
       <span class="title">{{news_title}}</span>
       <div class="writer">
         <img class="left" :src="news_writer_icon" :alt="news_writer_icon_alt">
@@ -15,10 +15,38 @@
         </div>
         <div class="right">关注</div>
       </div>
-      <span class="content">{{news_content}}</span>
+      <span class="content">{{news_details}}</span>
       <img class="photo" :src="news_photo" :alt="news_photo_alt">
     </div>
-    <div class="related_suggestion"></div>
+    <div class="related_suggestion">
+      <div class="top">
+        <span class="title">相关推荐</span>
+        <span class="see_more">打开今日头条看更多</span>
+      </div>
+      <div class="news" v-for="(item, index) of news_content" :key="index" @click="route_to('news_details')">
+        <div class="news_normal" v-if="item.show_news_normal">
+          <div class="news_top">
+            <div class="news_left">
+              <div class="news_title">{{item.news_title}}</div>
+              <div class="news_information">
+                <span class="news-sticky_content" v-if="item.show_sticky_content">{{item.sticky_content}}</span>
+                <div class="news-writer">{{item.writer}}</div>
+                <div class="news-number_of_comments">评论 {{item.number_of_comments}}</div>
+                <div class="news-release_time" v-if="item.show_release_time">{{item.release_time}}小时前</div>
+              </div>
+            </div>
+            <div class="news_right">
+              <div class="news-photo_outside">
+                <img class="news-photo" v-bind:src="item.photo">
+                <div class="play_button" v-if="item.show_play_button"></div>
+              </div>
+            </div>
+          </div>
+          <div class="news_bottom"></div>
+        </div>
+      </div>
+
+    </div>
     <div class="popular_discussion"></div>
     <div class="top_comments"></div>
   </div>
@@ -34,9 +62,25 @@ export default {
       news_writer_icon_alt: "新华社",
       news_writer_name: "新华社",
       news_writer_info: "2小时前 · 新华社官方账号",
-      news_content: "国家主席习近平11月10日在上海合作组织成员国元首理事会第二十次会议上发表重要讲话。",
+      news_details: "国家主席习近平11月10日在上海合作组织成员国元首理事会第二十次会议上发表重要讲话。",
       news_photo: "https://p3.pstatp.com/large/pgc-image/e7a638a1bc274bf5ac0eeea1e47c298a",
-      news_photo_alt: "国家主席习近平在上海合作组织成员国元首理事会第二十次会议上发表重要讲话"
+      news_photo_alt: "国家主席习近平在上海合作组织成员国元首理事会第二十次会议上发表重要讲话",
+      news_content: [
+        {
+          show_news_normal: true,
+          news_title: "人生没有下辈子，请珍惜当下的一切，让这一生，活得最美！",
+          show_sticky_content: false,
+          sticky_content: "置顶",
+          writer: "力量5201314",
+          number_of_comments: "8",
+          show_release_time: false,
+          release_time: "2",
+          photo: "https://p3-tt.bytecdn.cn/large/pgc-image/42cc5d49745c44249cbffb41f00aa49a",
+          show_play_button: false,
+          show_news_app_promotion: false,
+          show_news_advertisement: false
+        },
+      ],
     }
   },
   methods: {
@@ -92,7 +136,7 @@ export default {
       border-radius: 5px;
     }
   }
-  .news {
+  .news_detail {
     .title {
       font-size: 24px;
       font-weight: 800;
@@ -150,6 +194,159 @@ export default {
       max-width: 100%;
       max-height: 100%;
       margin: 0 5vw 0;
+    }
+  }
+  .related_suggestion {
+    .top {
+      display: flex;
+      align-items: center;
+      margin: 40px 18px 17px 18px;
+      .title {
+        flex: 1;
+        font-size: 17px;
+        color: #222;
+        font-weight: 900;
+      }
+      .see_more {
+        font-size: 12px;
+        color: #f04142;
+        font-weight: 400;
+      }
+      .see_more::after {
+        content: "";
+        display: inline-block;
+        width: 12px;
+        height: 12px;
+        background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACEAAAAkCAMAAAAw96PuAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAzUExURUxpcfA/QO8/P/BBQe8/P/BBQvBBQe8/P/BAQfBBQfBBQfBAQe8/P/BAQe8/Qu8/QvBBQnJqC2oAAAAQdFJOUwBgEM8w76Agf9+/kECvUHA6kNbiAAAAcklEQVR42tXSRw7DMAxEUUoiVdzy73/a3GBoIM7Cs34gwWL/iI8ETMK1OCF0lbFgaeJxi7QqSQE2k/kAPRsZ9pxMTfo9UqSoDbg0WRBVEk/7bMD4aZr5COjZdU8FDvSH5F821hMgPFulBLZDMZ1y2FvzBRIPBlByP+/+AAAAAElFTkSuQmCC) no-repeat 50%;
+        background-size: contain;
+        position: relative;
+        top: 1px;
+      }
+    }
+  }
+}
+
+.news {
+  padding: 10px 19px;
+  .news_normal {
+    .news_top {
+      display: flex;
+      .news_left {
+        flex: 1;
+        .news_title {
+          font-size: 17px;
+          color: #000;
+          font-weight: 400;
+        }
+        .news_information {
+          display: -ms-flexbox;
+          display: flex;
+          -ms-flex-align: center;
+          height: 28px;
+          span {
+            padding: 1px 3.5px;
+            font-size: 12px;
+            border: 1px solid rgba(248, 89, 89, 0.5);
+            color: #f85959;
+            height: 17px;
+          }
+          div {
+            padding: 1px 2.5px;
+            font-size: 12px;
+            height: 17px;
+            font-weight: 400;
+            color: #999;
+          }
+        }
+      }
+      .news_right {
+        max-width: 27vw;
+        .news-photo_outside {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          .news-photo {
+            width: 100%;
+          }
+          .play_button {
+            background: url(https://s3.pstatp.com/growth/mobile_list/image/playicon_video@3x_dcf536ff.png) no-repeat center center;
+            width: 7%;
+            height: 7%;
+            background-size: 100%;
+            display: block;
+            position: absolute;
+          }
+        }
+      }
+    }
+    .news_bottom {
+      border-bottom: 1px solid #F8F8F8;
+    }
+    
+  }
+  .news_app_promotion {
+    .top {
+      .promotion_photo {
+        width: 91vw;
+        padding: 20px 0 0 0;
+      }
+    }
+    .bottom {
+      font-size: 17px;
+      color: #000;
+      font-weight: 400;
+      display: flex;
+      height: 28px;
+      border-bottom: 1px solid #F8F8F8;
+      align-items: center;
+      .promotion_letter {
+        padding: 1px 3.5px;
+        font-size: 12px;
+        border: 1px solid rgba(248, 89, 89, 0.5);
+        color: #f85959;
+        height: 17px;
+      }
+      .promotion_name {
+        padding: 1px 2.5px;
+        font-size: 12px;
+        height: 17px;
+      }
+    }
+  }
+  .news_advertisement {
+    .top {
+      .ad_title {
+        font-size: 17px;
+        color: #000;
+        font-weight: 400;
+      }
+    }
+    .middle {
+      .ad_photo {
+        width: 91vw;
+      }
+    }
+    .bottom {
+      font-size: 17px;
+      color: #000;
+      font-weight: 400;
+      display: flex;
+      height: 28px;
+      border-bottom: 1px solid #F8F8F8;
+      align-items: center;
+      .ad_text {
+        padding: 1px 3.5px;
+        font-size: 12px;
+        border: 1px solid rgba(42, 144, 215, 0.5);
+        color: #2a90d7;
+        height: 17px;
+      }
+      div {
+        padding: 1px 2.5px;
+        font-size: 12px;
+        height: 17px;
+      }
     }
   }
 }
